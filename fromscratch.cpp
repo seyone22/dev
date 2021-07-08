09acca34afed1434abba2dfe0149c17990304333
S.G.Seyone <s.g.seyone@protonmail.com>
2021-07-08 14:56:48 +0530

    test2

--- fromscratch.cpp
+++ fromscratch.cpp
@@ -4,7 +4,7 @@ using namespace std; // this tells us the keywords we can use and the cm can und
 
 int main()
 {
-    cout << "test from scratch" << endl; //testing github pushing
+    cout << "test from scratch" << endl; //testing github pushing test2
     system("pause");
     return 0;
 }
