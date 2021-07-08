5df638148aa53794e5cfe74868f1ae1f16820995
S.G.Seyone <s.g.seyone@protonmail.com>
2021-07-08 14:52:05 +0530

    commit on 'master'

---
+++ fromscratch.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std; // this tells us the keywords we can use and the cm can understand
+
+int main()
+{
+    cout << "test from scratch" << endl;
+    system("pause");
+    return 0;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World"<<" "<<"test" << endl; //end line changes the cursor to another line
+    cout << "Name: Seyone" << endl << "ID: 2019/ICT/46" << endl;
+    system("pause");
+    return 0;
+}
