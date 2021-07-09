93169e77bf48a418fbc09cb2097040370c76484f
S.G.Seyone <s.g.seyone@protonmail.com>
2021-07-09 23:00:46 +0530

    yeh it doesn't work btw

---
+++ functions.py
@@ -0,0 +1,12 @@
+def get_name():
+    name = input("And your name is... >")
+
+def show_name(name):
+    print("The name is:" , name)
+
+show_name("Smash")
+show_name("UWU")
+show_name("The better UWU")
+
+get_name()
+show_name()
