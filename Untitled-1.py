e1cae830ce18afb85e71a06912180cc6939ef219
S.G.Seyone <s.g.seyone@protonmail.com>
2021-07-09 22:22:16 +0530

    bool

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,8 +1,11 @@
+from typing import TYPE_CHECKING
+
+
 print("Hello World!")
 
-x = 100000 #integer
-12.12 #float
-"Hello" #string
+x = 100000  # integer
+12.12  # float
+"Hello"  # string
 print(x)
 
 cookies = 10
@@ -10,3 +13,16 @@ days = 10
 
 cumulative = cookies * days
 print(cumulative)
+
+is_cookies_20 = cumulative == 20
+print(is_cookies_20)
+
+worship_satan = input("Have you paid your dues to the Lord? >")
+worship_satan_bool = (worship_satan == "yes")
+
+if worship_satan_bool:
+    print("Good job!")
+else:
+    print("Heathen!")
+
+print("Next statement")
