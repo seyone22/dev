8d4c01ac2e97a1a983273dd7236b1a50bef295fb
S.G.Seyone <s.g.seyone@protonmail.com>
2021-07-09 21:43:02 +0530

    night

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+
+x = 100000 #integer
+12.12 #float
+"Hello" #string
+print(x)
+
+cookies = 10
+days = 10
+
+cumulative = cookies * days
+print(cumulative)
---
+++ sauce_gen.cpp
@@ -0,0 +1,34 @@
+//Sauce generator created by A-kun
+//If you know what the codes are for, then you know.
+#include <iostream>
+
+using namespace std;
+void inputSelector(int& i)
+{
+    int i = 0;
+    cin >> i;
+}
+int main()
+{
+    cout << "Welcome to the Sauce Generator! What can we do for you? \n";
+    cout << "\t1.\tGenerate a Sauce\n";
+    cout << "\t2.\tExit the program\n";
+    int i = 0;
+    inputSelector(i);
+    if (inputSelector(i) == 1)
+    {
+        cout << "You have requested a code.";
+    }
+    else if (n == 2)
+    {
+        exit(0);
+    }
+    else
+    {
+    };
+
+    cout << n;
+    cout << endl;
+    system("pause");
+    return 0;
+}
