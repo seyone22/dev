5f86e4c1f556219e8692ad40734f0da8bfd8bb94
S.G.Seyone <s.g.seyone@protonmail.com>
2021-07-09 18:19:03 +0530

    base commit 9.7

---
+++ Untitled-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << rand();
+    system("pause");
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a; //initialize the integer a
+
+    cin >> a;
+    cout << "Hi, how old are you?" << endl;
+    cout << "I am " << a << " years old" << endl;
+    //system("pause");
+}
