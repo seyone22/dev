138a2228ffa48661a130d7cbb706125fbfcc8812
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-08-04 16:55:20 +0530

    changed eqn

--- problem_sheet_01/q08_eqn.cpp
+++ problem_sheet_01/q08_eqn.cpp
@@ -9,7 +9,7 @@ int main()
     {
         cout << "This program will tell you if your input is a solution for the equation 3x-15=0\nEnter your value below. ENTER ONLY NUMBERS. YOU HAVE BEEN WARNED.\n";
         cin >> input;
-        if (input == 15)
+        if (3*input - 15 == 0)
             cout << "Yeah, " << input << " is a solution to that equation.\n";
         else
             cout << "Nope. Not a solution. Try again.\n";
