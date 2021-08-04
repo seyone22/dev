49585c870e3e1c9f84b22f69f3db94dfb6c90ee0
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-08-04 11:29:32 +0530

    change to message

--- sorter.cpp
+++ sorter.cpp
@@ -9,7 +9,7 @@ int main()
     do
     {
         int input[3], temp;
-        cout << "This program will sort 3 input numbers alphabetically.\nPlease enter 3 numbers seperated by spaces. Do not enter special characters or numbers. The program will break.\n";
+        cout << "This program output the largest number from a given set of numbers.\nPlease enter 3 numbers seperated by spaces. Do not enter special characters or numbers. The program will break.\n";
         cin >> input[0] >> input[1] >> input[2];
         if (input[0] >= input[1])
         {
