7044d8000b8cac25254013ecc93594f05ba2972c
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-08-04 11:27:11 +0530

    sorter with if and else

--- sorter.cpp
+++ sorter.cpp
@@ -9,10 +9,22 @@ int main()
 {
     do
     {
-        char input[4];
-        cout << "This program will sort 4 input characters alphabetically.\nPlease enter four characters seperated by spaces. Do not enter special characters or numbers. The program will break.\n";
-        cin >> input[0] >> input[1] >> input[2] >> input[3];
-        sort(input, input + 4);
-        cout << input[4] << endl;
+        int input[3], temp;
+        cout << "This program will sort 3 input numbers alphabetically.\nPlease enter 3 numbers seperated by spaces. Do not enter special characters or numbers. The program will break.\n";
+        cin >> input[0] >> input[1] >> input[2];
+        if (input[0] >= input[1])
+        {
+            temp = input[0];
+        } else
+        {
+            temp = input[1];
+        }
+        if (input[2] >= temp)
+        {
+            temp = input[2];
+        }
+        cout << "the largest number is " << temp << endl;
     } while (true);
+    system("pause");
+    return 0;
 }
