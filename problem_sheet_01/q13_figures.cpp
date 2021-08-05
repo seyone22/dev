6f64de3d812002094765da50ae304d07069ebd45
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-08-05 09:36:09 +0530

    fully solved!!!

--- problem_sheet_01/q13_figures.cpp
+++ problem_sheet_01/q13_figures.cpp
@@ -63,6 +63,41 @@ int main()
         cout << endl;
     }
 
+    cout << endl;
+    for (int i = 0; i <= 6; i++)
+    {
+        for (int count = 6; count > i; count--)
+        {
+            cout << " ";
+        }
+        for (int count = 0; count < i; count++)
+        {
+            cout << "*";
+        }
+        for (int count = 0; count < i; count++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i <= 6; i++)
+    {
+        for (int count = 0; count < i; count++)
+        {
+            cout << " ";
+        }
+        for (int count = 6; count > i; count--)
+        {
+            cout << "*";
+        }
+        for (int count = 6; count > i; count--)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
     system("pause");
     return 0;
 }
