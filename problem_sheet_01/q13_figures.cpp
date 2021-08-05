2ba8fbea885a5932dbf05b9bf88a15cc35b05390
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-08-05 09:34:01 +0530

    third one success, albeit unexpectedly

--- problem_sheet_01/q13_figures.cpp
+++ problem_sheet_01/q13_figures.cpp
@@ -43,6 +43,26 @@ int main()
         cout << endl;
     }
 
+    cout << endl;
+
+    for (int i = 0; i <= 6; i++)
+    {
+        for (int count = 0; count < i; count++)
+        {
+            cout << " ";
+        }
+        for (int count = 6; count > i; count--)
+        {
+            cout << "*";
+        }
+        for (int count = 6; count > i; count--)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
     system("pause");
     return 0;
 }
