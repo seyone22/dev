3bf692424a1acaaed5858f168c55a6dcaf518d8f
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-08-05 09:00:27 +0530

    figures, first two done

---
+++ problem_sheet_01/q13_figures.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i <= 6; i++)
+    {
+        for (int count = 0; count < i; count++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        
+    }
+
+    cout << endl;
+
+        for (int i = 0; i <= 6; i++)
+    {
+        for (int count = 6; count > i; count--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        
+    }
+
+    system("pause");
+    return 0;
+}
