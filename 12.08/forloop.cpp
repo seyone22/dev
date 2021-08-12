e2a6bcf464a027dd5246b78efe63b2a643b968b4
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-08-12 08:11:57 +0530

    new loop

--- 12.08/forloop.cpp
+++ 12.08/forloop.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
+/*#include <iostream>
 
 using namespace std;
 
 int main()
 {
     for(int i=10;i>0;i--)
+    {
+        cout << i << endl;
+    }
+    cout << "End of Loop";
+}*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=3;i<=30;i=i+3)
     {
         cout << i << endl;
     }
