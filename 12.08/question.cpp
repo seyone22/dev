d6840aea412c21839eca35cc02e3ff23ef9e64b4
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-08-12 14:25:23 +0530

    handles 0 properly now

--- 12.08/question.cpp
+++ 12.08/question.cpp
@@ -8,12 +8,17 @@ int main()
     double input = 0, factorial = 1;
     cout << "Enter a number\n";
     cin >> input;
-    if(input <= 0)
+    if (input < 0)
     {
         cout << "Invalid input!";
         return 0;
     }
-    for(int i = 1; i <= input; i++)
+    else if (input == 0)
+    {
+        cout << "1";
+        return 0;
+    }
+    for (int i = 1; i <= input; i++)
     {
         factorial = i * factorial;
     }
