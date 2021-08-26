b82be06278ac51864e935f7172150456aa0f384f
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-08-26 15:27:05 +0530

    odd looper!

--- averageNumbers.cpp
+++ averageNumbers.cpp
@@ -1,7 +1,7 @@
 //Author: S.G.Seyone (2019/ICT/46)
 
 //This program will find the average of numbers entered by the user
-//If the number is negative, it will display the average of numbers entered till that point.
+//If the number is odd, it will display the average of numbers entered till that point.
 
 #include <iostream>
 
