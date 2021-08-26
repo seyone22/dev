f68b27c367fea070ebc213216d898eb897a8273b
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-08-26 15:30:54 +0530

    done!

--- averageNumbers.cpp
+++ averageNumbers.cpp
@@ -1,7 +1,7 @@
 //Author: S.G.Seyone (2019/ICT/46)
 
 //This program will find the average of numbers entered by the user
-//If the number is odd, it will display the average of numbers entered till that point.
+//If the number is negative, it will display the average of numbers entered till that point.
 
 #include <iostream>
 
@@ -9,19 +9,28 @@ using namespace std;
 
 int main()
 {
-    int input, runningTotal, looper = 0;
+    int input, runningTotal, looper = 0, average, i = 0;
+    string tester;
     do
     {
-        cout << "Enter a number\n";
-        cin >> input;
-        if (input % 2 == 0)
+        do
         {
-            runningTotal += input;
-        } else
-        {
-            looper = 1;
-            cout << "The running total is " << runningTotal << endl;
-        }
-    } while (looper == 0);
+            cout << "Enter a number\n";
+            cin >> input;
+            if (input > 0)
+            {
+                runningTotal += input;
+                i++;
+            }
+            else
+            {
+                looper = 1;
+                average = runningTotal / i;
+                cout << "The average of all positive numbers enteres so far is is " << average << endl;
+            }
+        } while (looper == 0);
+        cout << "Do you want to do another count? (Y/N)";
+        cin >> tester;
+    } while (tester == "T" || tester == "t");
     return 0;
 }
