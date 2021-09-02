11d1e6157413e807da534a5cbeed5d14d1674853
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-09-02 14:53:30 +0530

    done

---
+++ arrayduplicator.cpp
@@ -0,0 +1,34 @@
+//Author: S.G.Seyone (2019/ICT/46)
+
+//write a program to copy all the elements of one array to another array
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int input, counter, i = 0;
+    cout << "How many numbers will you enter?\n";
+    cin >> counter;
+    int numbers[counter]{};
+
+    do
+    {
+        cout << "Please enter a number\n";
+        cin >> input;
+        numbers[i] = input;
+        i++;
+    } while (i < counter);
+
+    int otherArray[counter];
+    for (int j = 0; j < counter; j++)
+    {
+        otherArray[j] = numbers[j];
+    }
+    for (int k = 0; k < counter; k++)
+    {
+        cout << otherArray[k] << endl;
+    }
+    return 0;
+}
