5a512fe4446532aefe31ab0f32eaba122c600a6d
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-09-08 10:41:32 +0530

    left rotator

---
+++ array_leftrotator.cpp
@@ -0,0 +1,43 @@
+//Author: S.G.Seyone (2019/ICT/46)
+
+//This program will left rotate any given array.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int input, counter, i = 0;
+    cout << "How many numbers will you enter?\n";
+    cin >> counter;
+    int numbers[counter]{};
+
+    do
+    {
+        cout << "Please enter a number\n";
+        cin >> input;
+        numbers[i] = input;
+        i++;
+    } while (i < counter);
+
+    int otherArray[counter];
+
+//snippet to right rotate an array. Basically it shifts everything over by one to the left, and it overflows to the nth index.
+    for (int j = 0; j < counter - 1; j++) //-1 because otherwise, it will overwrite the last index with numbers[5], which doesn't exist.
+    {
+        if (j == 0)
+        {
+            otherArray[counter - 1] = numbers[j];
+        }
+        otherArray[j] = numbers[j + 1];
+    }
+
+//snippet to output the array code
+    cout << "output\n";
+    for (int k = 0; k < counter; k++)
+    {
+        cout << otherArray[k] << endl;
+    }
+    return 0;
+}
