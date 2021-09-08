809eff3ace5c259dfe8f26f35adfb71ef3a983a3
seyone22 <61453289+seyone22@users.noreply.github.com>
2021-09-08 16:47:49 +0530

    stringsplitter

---
+++ array_Features.cpp
@@ -0,0 +1,23 @@
+//Author: S.G.Seyone (2019/ICT/46)
+
+//This program will left rotate any given array.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double arrayOne[3];
+    int arrayTwo[3];
+
+    cout << sizeof(arrayOne) << endl;
+    cout << sizeof(arrayTwo) << endl;
+
+    cout << endl;
+
+    cout << sizeof(arrayOne[1]) << endl;
+    cout << sizeof(arrayTwo[1]) << endl;
+
+    return 0;
+}
---
+++ splitting_strings.cpp
@@ -0,0 +1,47 @@
+//Author: S.G.Seyone (2019/ICT/46)
+
+//This program will left rotate any given array.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char fullName[] = "Seyone Gunasingham";
+
+    //try to get length of array using magic
+    int arrayLength = sizeof(fullName) - 1; //it gives 19 where 18 is length of string... weird. Just reduce one for calculation's sake.
+    int firstLength = 0; //secondLength not necesesary for two terms
+    char temp;
+
+    //gets the lengths of first and second name parts to memory
+    for (int i = 0; i < arrayLength; i++)
+    {
+        temp = fullName[i];
+        if (temp==' ')
+        {
+            break;
+        }
+        else
+        {
+            firstLength +=1;
+        }
+    }
+
+    cout << "First Name: ";
+    for (int i = 0; i < firstLength; i++)
+    {
+        cout << fullName[i];
+    }
+    cout << endl;
+
+    cout << "Second Name: ";
+    for (int i = firstLength + 1; i < arrayLength; i++)
+    {
+        cout << fullName[i];
+    }
+    
+    
+    return 0;
+}
