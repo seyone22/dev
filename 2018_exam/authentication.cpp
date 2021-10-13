40bdd58d16d239203c44436dde47a0c0ecb614ea
seyone <s.g.seyone@protonmail.com>
2021-10-13 16:13:28 +0530

    authentication push!

---
+++ 2018_exam/authentication.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+string getinput(string description = "No description provided")
+{
+    string input;
+    cout << description << "\n>>";
+    getline(cin, input);
+    return input;
+}
+
+int main()
+{
+    do
+    {
+        int attempts = 0;
+
+        if (attempts == 3)
+        {
+            return 0;
+        }
+        
+        string username = getinput("Please enter your username");
+        string pwd = getinput("Please enter your password");
+        string pwdCon = getinput("Please reenter your password");
+        if (pwd == pwdCon)
+        {
+            if (pwd == "0666")
+            {
+                cout << "Access granted. Good day, " << username << "!";
+                break;
+            }
+            else
+            {
+                cout << "The password is incorrect. Please try again.";
+                attempts+=1;
+            }
+        }
+        else
+        {
+            cout << "The passwords do not match. Please try again.";
+            attempts+=1;
+        }
+    } while (true);
+
+    cout << "successful break";
+    system("pause");
+    return 0;
+}
