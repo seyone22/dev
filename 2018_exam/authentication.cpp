efac1471ec964e5d978b948236927ab3dabd5572
seyone <s.g.seyone@protonmail.com>
2021-10-21 07:32:54 +0530

    clean

--- 2018_exam/authentication.cpp
+++ 2018_exam/authentication.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -14,37 +15,40 @@ string getinput(string description = "No description provided")
 int main()
 {
     int attempts = 0;
-    do
+    while (true)
     {
         if (attempts == 3)
         {
             return 0;
         }
-        
+
         string username = getinput("Please enter your username");
         string pwd = getinput("Please enter your password");
         string pwdCon = getinput("Please reenter your password");
+        int pwdInt = stoi(pwd);
         if (pwd == pwdCon)
         {
-            if (pwd == "0666") //set unique password to check.
+            if ((pwdInt >= 1000 && pwdInt <= 9999) || pwdInt == 0000) //checks all numerical values from 0000 to 1000
             {
-                cout << "Access granted. Good day, " << username << "!";
+                cout << "Access granted. Good day, " << username << "!" << endl;
                 break;
             }
             else
             {
-                cout << "The password is incorrect. Please try again.";
-                attempts+=1;
+                cout << "The password is incorrect. Please try again.\n";
+                attempts += 1;
+                cout << "Remaining attempts: " << attempts << endl;
             }
         }
         else
         {
-            cout << "The passwords do not match. Please try again.";
-            attempts+=1;
+            cout << "The passwords do not match. Please try again.\n";
+            attempts += 1;
+            cout << "Remaining attempts: " << attempts << endl;
         }
-    } while (true);
+    };
 
-    cout << "successful break";
+    cout << "successful break\n";
     system("pause");
     return 0;
 }
